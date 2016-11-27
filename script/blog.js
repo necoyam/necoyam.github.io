10d762aa00669ce60f11a1415eb80516147da088
necoyam <necoyam@gmail.com>
2016-11-27 14:34:06 +0000

    Upload for generator test

--- script/blog.js
+++ script/blog.js
@@ -1,9 +1,2 @@
 $(document).ready(function() {
-    $.ajaxSetup({ cache: true });
-	$.getScript('//connect.facebook.net/ko_KRsdk.js', function(){
-		FB.init({
-			appId: '190532464351842',
-			version: 'v2.7' 		
-		});     
-	});
-}
+});
